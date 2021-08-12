7960abf2ed03afcc61afb7740f8a0a2877f4a2c3
clementwahaga <c.wahaga@cci-formation.nc>
2021-08-12 16:52:28 +1100

    add all files

---
+++ helper/db.php
@@ -0,0 +1,18 @@
+<?php
+function pdo_connect_mysql()
+{
+  $username = 'root';
+  $password = "";
+
+  try {
+
+    $dbh = new PDO('mysql:host=localhost;dbname=sakila', $username, $password);
+    $dbh->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ATTR_ERRMODE);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    return $dbh;
+  } catch (PDOException $e) {
+    print "Erreur, vous avez etait deconnecté !: " . $e->getMessage() . "<br/>";
+    return false;
+  }
+}
