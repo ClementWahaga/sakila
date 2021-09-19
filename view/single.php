d4dbfcfee0be9fbd874247015f2f8e8839c84761
Clement Wahaga <c.wahaga@cci-formation.nc>
2021-09-19 05:28:16 +0200

    add style

--- view/single.php
+++ view/single.php
@@ -1,22 +1,24 @@
 <?php  
 $movie = new film($film_id='',$title='',$descritpion='',$release_year='',$language_id='',$original_language_id='',$rental_duration='',$length='',$replacement_cost='',$special_features='',$last_update='');
-$data = $movie->findOne(['film_id']);
+$data = $movie->findOne($film_id);
 
 ?>
-<?php
-echo '<pre>';
-   print_r($data);
-echo '</pre>';
-?>
 
 
-    <div class="neuro">
-        
+
+    <div class="single">
         <i class="fas fa-film"></i>
-        <p><?= $data->title?> </p>
-        <p>sorti en <?= $data->release_year ?></p>
-        <p>description: <?= $data->description ?></p>
-        <em>Durée du film : <?= $data->length ?> minutes</em>
+        <h3><?= $data->title?> </h3>
+        <div><h5>sorti en </h5><?= $data->release_year ?></div>
+        <p><h5>description:</h5><?= $data->description ?></p>
+        <em><h5>Durée du film : </h5><?= $data->length ?> minutes</em>
+        <div>
+            <form action="" methodo='POST'>
+                <button class="btn btn-success float-right">loué</button>
+                <button class="btn btn-warning float-right">rendre</button>
+            </form>
+        </div>
         
-     
+        
     </div>
+    
